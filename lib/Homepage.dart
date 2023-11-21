802f2795603a876f4e1d96fd0cc668e70c5742ef
tronout28 <fatihabdurahman28@gmail.com>
2023-11-21 10:08:56 +0700

    sejarah bengkulu

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'Historypage.dart';
 import 'Touristplace.dart';
 import 'Foodpage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -10,13 +12,51 @@ class HomePage extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
+            actions: <Widget>[
+              Row(children: [
+                TextButton(
+                  onPressed: () {
+                    Get.to(HistoryPage());
+                  },
+                  child: Text(
+                    "History",
+                    style: GoogleFonts.poppins(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Foods",
+                    style: GoogleFonts.poppins(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Ekonomi",
+                    style: GoogleFonts.poppins(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Culture",
+                    style: GoogleFonts.poppins(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                )
+              ]),
+            ],
             expandedHeight: 200.0,
             floating: false,
             pinned: true,
             flexibleSpace: FlexibleSpaceBar(
               title: Text('Bengkulu History'),
-              background: Image.network(
-                'https://example.com/your-background-image.jpg',
+              background: Image.asset(
+                'img/37de1e64c2d9cd7a3f9236cfc6db9007.jpg',
                 fit: BoxFit.cover,
               ),
             ),
@@ -29,7 +69,7 @@ class HomePage extends StatelessWidget {
                     padding: EdgeInsets.symmetric(vertical: 20.0),
                     child: Text(
                       'Welcome to Bengkulu',
-                      style: TextStyle(
+                      style: GoogleFonts.poppins(
                         fontSize: 24.0,
                         fontWeight: FontWeight.bold,
                       ),
@@ -43,11 +83,84 @@ class HomePage extends StatelessWidget {
           SliverList(
             delegate: SliverChildBuilderDelegate(
               (BuildContext context, int index) {
-                return ListTile(
-                  title: Text('Some Content'),
+                return Column(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      height: 300,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                          image: AssetImage(
+                              'img/1b1ad0b23d6f39f7d530ee1de654939a.jpg'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 26,
+                    ),
+                    Text(
+                      "Galeri Bengkulu",
+                      style: GoogleFonts.poppins(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                    SizedBox(
+                      height: 26,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 200,
+                          width: 260,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              image: AssetImage(
+                                "img/tari piring (1).jpg",
+                              ),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          height: 200,
+                          width: 260,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              image: AssetImage(
+                                "img/f25eb4ec60a63402d5db1d5cb77649da.jpg",
+                              ),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          height: 200,
+                          width: 260,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              image: AssetImage(
+                                "img/37de1e64c2d9cd7a3f9236cfc6db9007.jpg",
+                              ),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    )
+                  ],
                 );
               },
-              childCount: 20, // Adjust the count as needed
+              childCount: 1,
             ),
           ),
         ],
